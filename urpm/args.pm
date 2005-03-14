78e0695c7fbb60fa5be32bb77c8fbf2fd0e6803f
Rafael Garcia-Suarez <rgarciasuarez@mandriva.org>
2005-03-14 10:53:50 +0000

    Add a --summary option to urpmq. Patch by Michael Scherer, slightly reworked (bug 14513)

--- urpm/args.pm
+++ urpm/args.pm
@@ -190,6 +190,7 @@ my %options_spec = (
 	keep => \$options{keep},
 	list => \$options{list},
 	changelog => \$options{changelog},
+	summary => \$options{summary},
 	'list-media:s' => sub { $options{list_media} = $_[1] || 'all' },
 	'list-url' => \$options{list_url},
 	'list-nodes' => \$options{list_nodes},
