shlomif/urpmi

commit 374930bf6419bfa2e994fe3dc8eca2a89ae6127e

there is no reason search_packages should return multiple pkgs with same fullname when using {provides}, and should not otherwise
(will cleanup duplication in next commit)


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/urpmi/trunk@231535 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

urpm/select.pm
@@ -179,9 +179,12 @@ sub search_packages {
 			    $best = $_;
 			}
 		    }
-		    $packages->{$best->id} = 1;
-		    $urpm->{debug} and $urpm->{debug}("search_packages: found " . $best->fullname . " matching $v");
-		    $best->set_flag_skip(0); #- reset skip flag as manually selected.
+		    my @l = grep { $_->fullname eq $best->fullname } @$_;
+		    $packages->{join('|', map { $_->id } @l)} = 1;
+		    foreach my $pkg (@l) {
+			$urpm->{debug} and $urpm->{debug}("search_packages: found " . $pkg->fullname . " matching $v");
+			$pkg->set_flag_skip(0); #- reset skip flag as manually selected.
+		    }
 		}
 	    }
 	}

commit c2cb782044aef81fc7b1be14917fe8b679b31af1

- urpmq:   o fix option -a : display packages of all compatible archs (#36942)
this fixes another issue introduced in commit r231535 (urpmi 4.10.15,
2007-11-15), partially fixed in r231782 (2007-11-28)

the commit has 2 parts:
- ensure all compatible archs are kept when {all} option is used
- display one pkg per line in urpmq

urpm/select.pm
@@ -83,7 +83,7 @@ sub search_packages {
 	my @ids = split /\|/, $name2ids->{$v};
 
 	#- in case we have a substring match, we want individual selection (for urpmq --fuzzy)
-	$packages->{$_} = 1 foreach $result eq 'substring' ? @ids : $name2ids->{$v};
+	$packages->{$_} = 1 foreach $result eq 'substring' || $options{all} ? @ids : $name2ids->{$v};
 
 	foreach (@ids) {
 	    my $pkg = $urpm->{depslist}[$_] or next;
@@ -120,8 +120,12 @@ sub _search_packages {
 
 		delete $compats{0}; #- means not compatible
 		#- if there are pkgs matching arch, prefer them
+		if (%compats && !$options{all}) {
+		    my $best_arch = min(keys %compats);
+		    %compats = ($best_arch => $compats{$best_arch});
+		}
 		if (%compats) {
-		    @l = (@$noarch, @{$compats{min(keys %compats)}});
+		    @l = (@$noarch, map { @$_ } values %compats);
 		}
 
 		#- we assume that if there is at least one package providing