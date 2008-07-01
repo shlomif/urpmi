d99173d6e61042d0ba9c4a6372c9e2a47d3bf942
Pascal Rigaux <pixel@mandriva.com>
2008-07-01 10:47:51 +0000

    simplify rpmdb_to_synthesis() prototype

--- urpm/bug_report.pm
+++ urpm/bug_report.pm
@@ -5,9 +5,9 @@ use urpm::msg;
 
 
 sub rpmdb_to_synthesis {
-    my ($urpm, $synthesis, $root) = @_;
+    my ($urpm, $synthesis) = @_;
 
-    my $db = urpm::db_open_or_die($urpm, $root);
+    my $db = urpm::db_open_or_die($urpm, $urpm->{root});
     my $sig_handler = sub { undef $db; exit 3 };
     local $SIG{INT} = $sig_handler;
     local $SIG{QUIT} = $sig_handler;
