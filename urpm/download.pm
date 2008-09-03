975eb4c4b4d7dae030207afd1672c0f1faaf3dde
Pascal Rigaux <pixel@mandriva.com>
2008-09-03 17:02:53 +0000

    "nicer" debug/log message

--- urpm/download.pm
+++ urpm/download.pm
@@ -784,7 +784,7 @@ sub sync_rel {
 
     my @files = map { reduce_pathname("$medium->{url}/$_") } @$rel_files;
 
-    my $files_text = join(' ', map { url_obscuring_password($_) } @files);
+    my $files_text = join(' ', ($medium->{mirrorlist} && $urpm->{allow_metalink} ? ($medium->{mirrorlist}, $medium->{'with-dir'}) : url_obscuring_password($medium->{url})), @$rel_files);
     $urpm->{debug} and $urpm->{debug}(N("retrieving %s", $files_text));
 
     eval { 
