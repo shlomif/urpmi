73b5c7c1ee2c69f6fed3aca51e44abf1447b17d0
Christophe Fergeau <cfergeau@mandriva.com>
2009-08-20 09:51:11 +0000

    add new line after message suggesting to remove orphans
    Fixes bug #53003

--- urpm/orphans.pm
+++ urpm/orphans.pm
@@ -359,8 +359,8 @@ sub get_now_orphans_msg {
     my $orphans = get_orphans($urpm);
     my @orphans = map { scalar $_->fullname } @$orphans or return '';
 
-    P("The following package:\n%s\nis now orphaned, if you wish to remove it, you can use \"urpme --auto-orphans\"",
-      "The following packages:\n%s\nare now orphaned, if you wish to remove them, you can use \"urpme --auto-orphans\"",scalar(@orphans), add_leading_spaces(join("\n", sort @orphans)));
+    P("The following package:\n%s\nis now orphaned, if you wish to remove it, you can use \"urpme --auto-orphans\"\n",
+      "The following packages:\n%s\nare now orphaned, if you wish to remove them, you can use \"urpme --auto-orphans\"\n",scalar(@orphans), add_leading_spaces(join("\n", sort @orphans)));
 }
 
 
