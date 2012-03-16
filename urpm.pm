8934ed0e04131f8ee2bf97e11f109fe039d079cb
Thierry Vignaud <tv@mageia.org>
2012-03-16 20:18:00 +0000

    enforce GC' style

--- urpm.pm
+++ urpm.pm
@@ -295,7 +295,7 @@ sub db_open_or_die_ {
     my $db;
     if ($urpm->{env_rpmdb}) {
 	#- URPM has same methods as URPM::DB and empty URPM will be seen as empty URPM::DB.
-        $db = new URPM;
+        $db = URPM->new;
         $db->parse_synthesis($urpm->{env_rpmdb});
     } else {
 	$db = db_open_or_die($urpm, $urpm->{root}, $b_write_perm);
