shlomif/urpmi

commit 2c409a005e0f974f4b6b4548c7a434367db8c967

Downgrade cryptic log message to debug message, fixes #49226
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/urpmi/trunk@255088 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

urpm/media.pm
@@ -643,7 +643,7 @@ sub parse_media {
 
 	if ($_->{searchmedia}) {
 	    $urpm->{searchmedia} = 1;
-	    $urpm->{log}(N("Search start: %s end: %s", $_->{start}, $_->{end}));
+	    $urpm->{debug}("Search start: %s end: %s", $_->{start}, $_->{end});
 	}
 
         $< == 0 and _generate_medium_names($urpm, $_);

commit a2d7908b4131602ffdfa61eb229af00ac00f9345

(parse_media) fix debug statement to actually output values
also tag it as translatable (sprintf() could have been used)

bug introduced by cfergeau in r255088 on Mar 30 2009 which
gratuitously drop the N() call:
"Downgrade cryptic log message to debug message, fixes #49226"

urpm/media.pm
@@ -716,7 +716,7 @@ sub parse_media {
 
 	if ($_->{searchmedia}) {
 	    $urpm->{searchmedia} = 1;
-	    $urpm->{debug} and $urpm->{debug}("Search start: %s end: %s", $_->{start}, $_->{end});
+	    $urpm->{debug} and $urpm->{debug}(N("Search start: %s end: %s", $_->{start}, $_->{end}));
 	}
 
         $< == 0 and _generate_medium_names($urpm, $_);