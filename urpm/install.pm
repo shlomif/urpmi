308ce1b7676b1a0e07a26327485fdc7892156fad
Thierry Vignaud <tv@mageia.org>
2012-12-15 02:04:03 +0000

    (install) do not show empty information from README* with --justdb

--- urpm/install.pm
+++ urpm/install.pm
@@ -348,7 +348,7 @@ sub install {
 	    }
 	}
 
-	if ($options{verbose} >= 0) {
+	if ($options{verbose} >= 0 && !$options{justdb}) {
 	    foreach (keys %{$urpm->{readmes}}) {
 		$urpm->{print}("-" x 70 .  "\n" .
                                  N("More information on package %s", $urpm->{readmes}{$_}));
