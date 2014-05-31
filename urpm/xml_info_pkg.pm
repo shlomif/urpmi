d74a214bbaddfb5dffdcef3001949c40796ce03a
Pascal Terjan <pterjan@gmail.com>
2014-05-31 14:21:31 +0000

    add some missing methods, allows to do urpmf --qf '%sourcerpm:%requires'

--- urpm/xml_info_pkg.pm
+++ urpm/xml_info_pkg.pm
@@ -52,6 +52,11 @@ sub epoch     { $_[0]{pkg}->epoch }
 sub buildhost { $_[0]{pkg}->buildhost }
 sub packager  { $_[0]{pkg}->packager }
 sub summary   { $_[0]{pkg}->summary }
+sub conflicts { $_[0]{pkg}->conflicts }
+sub obsoletes { $_[0]{pkg}->obsoletes }
+sub provides  { $_[0]{pkg}->provides }
+sub requires  { $_[0]{pkg}->requires }
+sub suggests  { $_[0]{pkg}->suggests }
 
 
 =item url($pkg)
