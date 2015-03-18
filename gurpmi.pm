670a062a01fc385fc234fd9ce6d0d48b4ffcb3a7
Thierry Vignaud <thierry.vignaud@gmail.com>
2015-03-18 17:45:47 +0100

    install perl-Glib-Object-Introspection...
    ...as priority upgrades prior to restart

--- gurpmi.pm
+++ gurpmi.pm
@@ -48,7 +48,7 @@ on a Mageia Linux distribution.
 
 =cut
 
-urpm::select::add_packages_to_priority_upgrade_list('gurpmi', 'perl-Glib', 'perl-Gtk3');
+urpm::select::add_packages_to_priority_upgrade_list(qw(gurpmi perl-Glib perl-Glib-Object-Introspection perl-Gtk3));
 
 sub usage () {
     print urpm::args::copyright('gurpmi', [ '1999-2010', 'Mandriva' ])
