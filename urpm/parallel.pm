shlomif/urpmi

commit ab62a489340294a8cf0f4960b8201b07f2e29bb3

drop very old CVS/SVN $Id tags

urpm/parallel.pm
@@ -1,4 +1,3 @@
-package urpm::parallel; # $Id: parallel.pm 250871 2009-01-06 17:21:05Z pixel $
 
 use strict;
 use urpm;

gurpmi.pm
@@ -2,7 +2,6 @@ package gurpmi;
 
 #- Copyright (C) 2005 MandrakeSoft SA
 #- Copyright (C) 2005-2010 Mandriva SA
-#- $Id: gurpmi.pm 271299 2010-11-21 15:54:30Z peroyvind $
 
 #- This is needed because text printed by Gtk3 will always be encoded
 #- in UTF-8; we first check if LC_ALL is defined, because if it is,

urpm.pm
@@ -1,6 +1,5 @@
 package urpm;
 
-# $Id: urpm.pm 271301 2010-11-22 00:50:49Z eugeni $
 
 no warnings 'utf8';
 use strict;

urpm/args.pm
@@ -1,6 +1,5 @@
 package urpm::args;
 
-# $Id: args.pm 271299 2010-11-21 15:54:30Z peroyvind $
 
 use strict;
 use warnings;

urpm/bug_report.pm
@@ -1,4 +1,3 @@
-package urpm::bug_report; # $Id: bug_report.pm 250871 2009-01-06 17:21:05Z pixel $
 
 use strict;
 use urpm;

urpm/cdrom.pm
@@ -1,6 +1,5 @@
 package urpm::cdrom;
 
-# $Id: cdrom.pm 257782 2009-06-05 14:51:02Z cfergeau $
 
 use strict;
 use urpm::msg;

urpm/cfg.pm
@@ -1,6 +1,5 @@
 package urpm::cfg;
 
-# $Id: cfg.pm 271299 2010-11-21 15:54:30Z peroyvind $
 
 use strict;
 use warnings;

urpm/download.pm
@@ -1,6 +1,5 @@
 package urpm::download;
 
-# $Id: download.pm 271299 2010-11-21 15:54:30Z peroyvind $
 
 use strict;
 use urpm::msg;

commit e22070d7a48ba33abd4e2d2ef6ad8a82daa2ec9c

fix urpm::parallel
breakage in commit ab62a489340294a8cf0f4960b8201b07f2e29bb3

urpm/parallel.pm
@@ -1,3 +1,4 @@
+package urpm::parallel;
 
 use strict;
 use urpm;