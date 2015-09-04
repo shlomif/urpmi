8b8a7f6ec72f62b799aae45a9a56fbcc5560677f
Thierry Vignaud <thierry.vignaud@gmail.com>
2015-09-04 05:09:27 -0400

    8.08

--- urpm.pm
+++ urpm.pm
@@ -14,7 +14,7 @@ use urpm::md5sum;
 # perl_checker: require urpm::media
 # perl_checker: require urpm::parallel
 
-our $VERSION = '8.07';
+our $VERSION = '8.08';
 our @ISA = qw(URPM Exporter);
 our @EXPORT_OK = ('file_from_local_url', 'file_from_local_medium', 'is_local_medium');
 
